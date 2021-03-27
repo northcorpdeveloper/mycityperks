f0dd4716c99992884c23644cc6b647fb8ad61a5f
Lenovo <Lenovo@DESKTOP-FPJ6CRC>
2021-03-27 18:00:15 +0530

    added files for

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -7,12 +7,24 @@ use App\Http\Controllers\Controller;
 use App\Models\Event;
 use App\Models\Team;
 use Auth;
-
+use App\Models\Product;
+use App\Models\Popular_category;
+use DB;
+use App\Quotation;
 class DashboardController extends Controller
 {
     public function index()
     {
-        $data=array('aa','ss');      
-        return view('user.dashboard.index', compact('data'));
+        //$routeName = Route::currentRouteName();							
+	$userId= Auth::user()->id;
+                                                         
+                                                         
+        $PopularCategory = DB::table('tbl_category')->get();
+        $content_createrData = DB::table('users')->where('id','!=',$userId)->where('user_type',1)->get();
+        $allcount = Product::count();
+        //$productList = DB::table('products')->join('users', 'users.id', '=', 'products.user_id')->select('products.*', 'users.name as username')->limit(8)->get()->toArray();
+        
+        //$data=array('aa','ss');      
+        return view('user.dashboard.index', compact('PopularCategory','content_createrData','allcount'));
     }
 }
